e60f6e879d3a6e1df020109db7ca29d7fda9dd77
Pinkymus <katinkaspangtoft@gmail.com>
2024-06-27 15:41:32 +0200

    NOTIFY LISTENERS

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -19,6 +19,8 @@ class Game extends ChangeNotifier {
     }
     currentTurn = lobby.users.first;
 
+    lobby.addListener(notifyListeners);
+
     conn.handlers["SetTurn"] = (data) {
       var lobbyID = data["lobby_id"];
       if (lobbyID != lobby.pin) {
